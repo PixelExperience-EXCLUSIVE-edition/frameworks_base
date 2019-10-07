5d9d3cd7ac746a6e8c44c571a6d0cc3bb128e58e
darkobas <darkobas@gmail.com>
2019-10-07 20:31:48 +0200

    base: add option to enable AOD on charging only [1/2]
    idoybh edits: Adapted to A11's settings backup
    [jhonboy121]: adapt to A12 changes in DozeParameters. Also in the og commit DOZE_ALWAYS_ON was being read from System namespace instead of Secure, fixed it here. Make use of SettingsProxy utlity classes for settings / getting values
    
    Change-Id: I831583fde68de15788e3d7ecab55d864726d140f
    
    Commit message #2:
    
    base: check whether device is charging when alwaysOnChargingEnabled is called
    
    * Using settings for this is quite redundant. And if you toggle aod on charge after plugging in then aod won't turn on unless you unplug and plug again (since DOZE_ON_CHARGE_NOW is set only if aod on charge is enabled and device is plugged in"
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Commit message #3:
    
    base: fix deadlock between activity manager and power manager
    
    * Using the battery manager intent to query plugged in status was the root cause of deadlock,
      so inside power manager, user mIsPowered and setting value instead of using the intent based power status
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    [jhonboy121]: adapted to A13
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I5fefb3d15ec3585803d293c7c6f56298704a7387
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11761,6 +11761,12 @@ public final class Settings {
          */
         public static final String LOCK_SCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
 
+       /**
+         * Whether to enable DOZE only when charging
+         * @hide
+         */
+        public static final String DOZE_ON_CHARGE = "doze_on_charge";
+
         /**
          * Enable udfps detection even when screen is off
          * Default value is 0
