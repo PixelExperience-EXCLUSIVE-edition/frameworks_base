b7864ea6d3c71aa9787e583fd8da5d129be300db
jhenrique09 <jhenrique09@users.noreply.github.com>
2022-01-18 20:46:03 +0800

    base: Implement cutout force full screen [1/3]
    Inspired by MIUI and Essential
    
    CutoutFullscreenController: Adapted from https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
    
    [cjh1249131356]: Adapt to A12
    
    Author: Jesse Chan <jc@lineageos.org>
    Date:   Sat, 21 Apr 2018 23:45:57 -0700
    
        Add an option to force pre-O apps to use full screen aspect ratio
    
        When an app target pre-O releases, the default max aspect ratio
        is 1.86:1 which leads to ugly black areas on devices that have
        screens with higher aspect ratio (for example Galaxy S8/S9).
    
        This change adds an option to allow users to change aspect ratio
        for pre-O apps to full screen aspect ratio.
    
        Change-Id: I41d5408841593a12443be885e11959bffaebb67b
    
    Change-Id: Ia93a550cc952962de4fa270aae37a7b8c9ba45ac
    Signed-off-by: userariii <abhattacharjee717@gmail.com>
    
    fi

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -269,6 +269,7 @@ import com.android.server.statusbar.StatusBarManagerInternal;
 import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.internal.util.custom.PixelPropsUtils;
+import com.android.internal.util.custom.cutout.CutoutFullscreenController;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -780,6 +781,8 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     private int mDeviceOwnerUid = Process.INVALID_UID;
 
+    private CutoutFullscreenController mCutoutFullscreenController;
+
     private final class SettingObserver extends ContentObserver {
         private final Uri mFontScaleUri = Settings.System.getUriFor(FONT_SCALE);
         private final Uri mHideErrorDialogsUri = Settings.Global.getUriFor(HIDE_ERROR_DIALOGS);
@@ -875,6 +878,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     public void installSystemProviders() {
         mSettingsObserver = new SettingObserver();
+
+        // Force full screen for devices with cutout
+        mCutoutFullscreenController = new CutoutFullscreenController(mContext);
     }
 
     public void retrieveSettings(ContentResolver resolver) {
@@ -6874,6 +6880,12 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
                 activity.restartProcessIfVisible();
             }
+        }
+    }
+
+    public boolean shouldForceCutoutFullscreen(String packageName) {
+        synchronized (this) {
+            return mCutoutFullscreenController.shouldForceCutoutFullscreen(packageName);
         }
     }
 }
